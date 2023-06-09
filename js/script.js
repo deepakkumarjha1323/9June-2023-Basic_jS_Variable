1a205172c1f4273e480bf38c33d55c8d78253aff
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-09 17:06:22 +0530

    jscodeproject

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// With Let you can Not do this
+
+// let x = 1000;
+ // then
+
+//  Let x = 2000;
+
+//  console.log(x); // Statement
+
+// Global Scope
+// var a = 200; //Global Variable
+
+// {
+//     // Block Scope
+//     console.log(a);
+// }
+// {
+//     let b = 300; // Local Varianle
+// }
+// console.log(b);
+
+// {
+//     var c =500;
+// }
+// console.log(c); // Statement
+
+// {
+//     let a = 20;
+//     let c = 50;
+
+//     console.log(a+c);
+// }
+
+// News Section Start
+
+let a = "The";
+let b = 'Punjab';
+let c = `excise`;
+let d = "department";
+let e = 'and';
+let f = `police`;
+let g = "recovered";
+let h = '17,000';
+let i = `kg`;
+let j = "of";
+let k = 'lahan';
+let l = `used`;
+let m = "in";
+let n = 'making';
+let o = `illicit`;
+let p = "liquor";
+let q = 'during';
+let r = `searches`;
+let s = "carried";
+let t = 'out';
+let u = `at`;
+let v = "Dasuya";
+let w = 'in';
+let x = `Hoshiarpur`;
+let y = "district";
+let z = 'over.';
+
+console.log(a+' '+b+" "+c+" "+d+" "+e+` `+f+' '+g+' '+h+' '+i+" "+j+` `+k+' '+l+" "+m+' '+n+" "+o+" "+p+' '+q+` `+r+' '+s+" "+t+" "+u+' '+v+` `+w+" "+x+" "+y+` `+z)
+
+
+// News Section End
